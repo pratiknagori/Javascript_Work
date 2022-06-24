1bd51a56584a19f72f80d6050c2a565cafd8ae31
pratik <pratiknagori@gmail.com>
2022-06-24 12:43:35 +0530

    Added more Java code

--- Java_work/Projects/MegaBytesConverter/src/Main.java
+++ Java_work/Projects/MegaBytesConverter/src/Main.java
@@ -3,6 +3,16 @@ public class Main {
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
         MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
         MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+        boolean isBarking = MegaBytesConverter.shouldWakeUp(true, 7);
+        System.out.println(isBarking);
+        boolean isItALeapYear = MegaBytesConverter.isLeapyear(2021);
+        System.out.println(isItALeapYear);
+        boolean isDoubleEqual = MegaBytesConverter.areEqualByThreeDecimalPlaces(3.147, 3.1476);
+        System.out.println(isDoubleEqual);
+        boolean isEqualSum = MegaBytesConverter.hasEqualSum(1, 1, 1);
+        System.out.println(isEqualSum);
+        boolean isNumberTeen = MegaBytesConverter.hasTeen(1, 15, 1);
+        System.out.println(isNumberTeen);
 
     }
 }
--- Java_work/Projects/MegaBytesConverter/src/MegaBytesConverter.java
+++ Java_work/Projects/MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,5 +1,5 @@
 public class MegaBytesConverter {
-    public static void printMegaBytesAndKiloBytes (int kiloBytes) {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         if (kiloBytes < 0) {
             System.out.println("Invalid Value");
         } else {
@@ -8,5 +8,62 @@ public class MegaBytesConverter {
             System.out.println(kiloBytes + " KB = " + megaBytes + "MB and " + remainingKilobytes + "KB");
         }
 
+    }
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (((hourOfDay < 8) && (hourOfDay >= 0) || (hourOfDay == 23)) && (barking == true)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // finding if a year is a leap year or not
+    public static boolean isLeapyear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 4 == 0 && year % 100 !=0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 ==0 && year % 400 ==0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces (double numOne, double numTwo) {
+        numOne = (int) (numOne * 1000);
+        System.out.println(numOne);
+        numTwo = (int) (numTwo * 1000);
+        System.out.println(numTwo);
+        if ( numOne == numTwo) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasEqualSum (int paramOne, int paramTwo, int paramThree) {
+        if ((paramOne + paramTwo) == paramThree) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasTeen (int numberOne, int numberTwo, int numberThree) {
+        if ((numberOne >= 13 && numberOne <=19) || (numberTwo >= 13 && numberTwo <=19) || (numberThree >= 13 && numberThree <=19)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isTeen (int numberOne) {
+        if (numberOne >= 13 && numberOne <=19) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
